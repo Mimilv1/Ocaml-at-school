8e3395a441b13ec62b00699611f9610f053c4774
Emilien Lambin <emilien.lambin@laposte.net>
2025-04-27 20:52:23 +0200

    Delete Ocaml/Exercice5.ml

--- Ocaml/Exercice5.ml
+++
@@ -1,8 +0,0 @@
-let n = int_of_string Sys.argv.(1) in
-let f:in_channel = open_in "./pi_1_million.txt" in
-for i=0 to n-2 do
-  input_char f
-done;
-let l:char = input_char f in
-Printf.printf "%c" l;;
-close_in f;;
